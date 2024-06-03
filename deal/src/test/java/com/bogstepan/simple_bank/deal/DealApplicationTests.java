ceff1116f0f3e598cea47e22f3bfa4ff07271d0e
Stepan Bogdanov <bogstepan@gmail.com>
2024-06-03 19:27:47 +0300

    Init deal

---
+++ deal/src/test/java/com/bogstepan/simple_bank/deal/DealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogstepan.simple_bank.deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
